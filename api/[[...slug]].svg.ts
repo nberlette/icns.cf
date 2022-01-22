9a99b3f99b1544bb7909f49b731a4a17e2e3e3de
Nicholas Berlette <nick@berlette.com>
2022-01-22 00:00:01 -0800

    update page layout, add clipboard function, geist-ui

--- api/[[...slug]].svg.ts
+++ api/[[...slug]].svg.ts
@@ -1,8 +1,10 @@
 import type {
   VercelRequest as Request,
-  VercelResponse as Response
+  VercelResponse as Response,
+  VercelRequestQuery as RequestQuery
 } from '@vercel/node';
 import type { SimpleIcon } from 'simple-icons';
+
 import icons from 'simple-icons';
 import tc from 'tinycolor2';
 
@@ -10,7 +12,7 @@ import tc from 'tinycolor2';
  * Converts a brand title into a slug/filename.
  * @param {String} title The title to convert
  */
-export function toSlug (title: string): string {
+function toSlug (title: string): string {
   return title.toLowerCase()
     .replace(/\+/g, "plus")
     .replace(/\./g, "dot")
@@ -34,7 +36,7 @@ export function toSlug (title: string): string {
  * @returns {string} the rewritten svg string
  */
 
-function toSVG(svg: string, props: Record<string, unknown> = {}): string {
+function toSVG(svg: string, props: Record<string, any> = {}): string {
   let properties = Object.assign({},
     {
       'xmlns': 'http://www.w3.org/2000/svg',
@@ -44,34 +46,33 @@ function toSVG(svg: string, props: Record<string, unknown> = {}): string {
     props,
     { 'fill': tc(props.fill).toHex8String() }
   );
-  return svg.replace(/^<svg[^>]+>/, `<svg ${<String>(Object.entries(properties).map(([k, v]) => `${k}="${v}"`).join(' '))}>`);
+  return svg.replace(/^<svg[^>]+>/, `<svg ${<string>(Object.entries(properties).map(([k, v]) => `${k}="${v}"`).join(' '))}>`);
 }
 
+/*
 declare const DAYS: number;
 declare const CACHE_BROWSER: number;
 declare const CACHE_CDN: number;
 declare const CONTENT_TYPE: string;
+*/
 
-interface RequestQueryParams {
-  slug: string;
-  name?: string;
-  color?: string;
-  type?: 'svg' | 'png' | null;
-};
+declare type QueryParams = {
+  slug?: string,
+  name?: string,
+  color?: string,
+  type?: string,
+} & RequestQuery;
 
 const DAYS = 60 * 60 * 24; 		// 86400 = 1 day
 const CACHE_BROWSER = 0.01 * DAYS; 	// 86400 * 7 days
 const CACHE_CDN = 0.01 * DAYS;		// 86400 * 5 days
 const CONTENT_TYPE = 'image/svg+xml;charset=utf-8';
 
-res.prototype.cors = function cors (res: Response = this) {
+function preflight (res: Response) {
   res.setHeader("Access-Control-Allow-Origin", '*');
   res.setHeader("Access-Control-Allow-Methods", '*');
   res.setHeader("Access-Control-Allow-Headers", '*');
-}
 
-res.prototype.preflight = function preflight (res: Response = this) {
-  res.cors();
   res.setHeader('Content-Type', CONTENT_TYPE);
   if (process.env.NODE_ENV !== 'production') {
     res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=0, must-revalidate');
@@ -80,26 +81,26 @@ res.prototype.preflight = function preflight (res: Response = this) {
   res.setHeader('Cache-Control', `public, max-age=${CACHE_BROWSER}, s-maxage=${CACHE_CDN}, immutable`);
 }
 
-export default async function handler (req: Request, res: Response): any {
+export default async function handler (req: Request, res: Response): Promise<any> {
   let {
     slug,
     name,
     color = 'default',
     type = 'svg'
-  }: RequestQueryParams = req.query;
+  }: QueryParams = req.query;
 
   // set cache-control and other headers before takeoff
-  res.preflight();
+  preflight(res);
   
   // res.send() helper function
-  const send = (data: any, code = 200, type = 'image/svg+xml;charset=utf-8') => {
+  const send = (data: any, code = 200, type = 'image/svg+xml;charset=utf-8'): any => {
     res.setHeader('Content-Type', type);
     console.log("[HTTP][%d][%s][%s]: %s", code, req.method, type, req.url);
     return res.status(code).send(data);
   }
 
   // json helper function
-  const json = (data: any, code = 200, spaces = 2) => {
+  const json = (data: any, code = 200, spaces = 2): any => {
     return send(JSON.stringify(data, null, spaces), code, 'application/json;charset=utf-8')
   }
 
@@ -109,18 +110,24 @@ export default async function handler (req: Request, res: Response): any {
 
   console.log(JSON.stringify(req.query, null, 0))
 
-  if (~slug.indexOf('.')) [slug,  type] = slug.split('.');
-  if (~slug.indexOf('/')) [color, slug] = slug.split('/');
-  if (~slug.indexOf('-')) [slug, color] = slug.split(/[-_]{1}/);
+  let _slug: string = slug || name || '';
+  let _type: string = type || 'svg';
+  let _color: string = color || 'default';
+
+  //if (~slug.indexOf('.')) [slug,  type] = slug.split('.') || [_slug, _type];
+  //if (~slug.indexOf('/')) [color, slug] = slug.split('/') || [_color, slug];
+  if (~_slug.indexOf('-')) [slug, color] = _slug.split('-') || [slug, color];
 
   let icon: SimpleIcon = icons.Get(slug);
   let icon_alt: SimpleIcon = icons.Get(color);
   
-  if (!icon && icon_alt) icon = icon_alt;
-  if (!icon) return json({ statusText: "Not found", status: 404 }, 404);
+  if ((!icon || typeof icon == 'undefined') && (!!icon_alt && typeof icon_alt != 'undefined')) 
+	  icon = icon_alt;
+  if (!icon || typeof icon == 'undefined')
+	  return json({ statusText: "Not found", status: 404 }, 404);
   
   const fill: string = (tc(color).isValid() && color != 'default' ? color : icon.hex);
-  const response = toSVG(icon.svg, { fill })
+  const response: string = toSVG(icon.svg, { fill });
 
   return send(response, 200);
 }
---
+++ api/index.ts
@@ -0,0 +1,124 @@
+import type { 
+	VercelResponse as Response, 
+	VercelRequest as Request, 
+	VercelRequestQuery as RequestQuery 
+} from '@vercel/node';
+import icons, { SimpleIcon } from 'simple-icons';
+import tc from 'tinycolor2';
+import { marked } from 'marked';
+
+const randomIconsCount = 10;
+try { delete icons.Get; } catch {}
+const randomIcons: any[] = Object.values(icons).sort((a,b) => Math.random() > 0.5 ? -1 : 1).slice(0, randomIconsCount)
+
+const homepage: string = `
+<!DOCTYPE html>
+<html>
+<head>
+<title>icns.ml • simpleicons as a service</title>
+<link rel="stylesheet" href="https://unpkg.com/@geist-ui/style/dist/style.css" />
+<style type="text/css">
+.markdown-body { max-width: 960px } 
+body { 
+	max-width: 70vw; margin: 2rem auto; 
+} 
+body > div {
+  margin: 1em 0;
+}
+pre.icon-url { 
+  margin-right: 2em; 
+} 
+pre.url {
+  transition: all 0.5s ease-in;
+  border: 2px solid #ddd;
+}
+pre.success {
+  border: 2px solid var(--zeit-success);
+}
+pre.success::after { 
+  content: "✅";
+  position:fixed;
+  right:0;
+  top:50%;
+  transform:translate(0,-50%);
+  margin: 0 1em; 
+}
+</style>
+</head>
+<body>  
+<div align=center>
+  <h1><img src="https://icns.ml/simpleicons.svg" width="32" alt="SimpleIcons" /> <code>icns.ml</code></h1>
+  <p><h4>The <a href="https://simpleicons.org" target="_blank" rel="noopener noreferrer">simpleicons</a> API + Vercel's Edge CDN.</h4></p>
+</div>
+<br>
+
+<div class="zi-card pin">
+
+## Schema  
+
+<pre class="zi-dark">
+  https://icns.ml/ :name [-:color] [.:type]
+</pre>
+<pre class="zi-dark">
+  https://icns.ml/ [:color] / :name [.:type]
+</pre>
+
+> Example of the recommended usage: **\`https://icns.ml/github.svg\`**
+
+- **\`:name\`**   · **\`required\`** · alphanumeric, no spaces, dashes, no special chars.  
+- **\`:color\`** · \`optional\` · default is brand's primary color.  
+- **\`:type\`**   · \`optional\` · default is \`svg\`.    
+- Specify just the logo slug and it will be filled with the brand color.  
+
+</div>
+<div class="zi-card">
+
+### Schema Examples
+
+<pre class="zi-dark">
+https://icns.ml/audi.svg
+https://icns.ml/twitter-white.svg
+https://icns.ml/white/twitter.svg
+https://icns.ml/ffffff/twitter.svg
+https://icns.ml/adobe
+</pre> 
+
+</div>
+
+<div class="zi-card">
+
+<table width="100%" cellpadding="2" cellspacing="2" class="zi-table">
+${randomIcons.map((icon: SimpleIcon): string => {
+        let iconUrl = `https://icns.ml/${icon.slug}.svg`;
+        return `<tr><td width="72">
+        <a href="${iconUrl}" target="_blank" rel="noopener noreferrer"><img src="${iconUrl}" alt="${icon.title}" aria-label="${icon.title}" width="72" height="72" class="icon" id="icon-${icon.slug}" /></a>
+        </td>
+        <td>
+            <a href="javascript:;" id="copy-${icon.slug}" class="copyurl" data-url="${iconUrl}" title="Click to copy URL to clipboard" style="position:relative" onclick="navigator.clipboard.writeText(this.getAttribute('data-url')).then(()=>{this.firstChild.classList.toggle('success'); setTimeout(() => this.firstChild.classList.toggle('success', false), 2000); alert('Copied to clipboard!'); })">
+	    <pre
+                id="url-${icon.slug}"
+                class="zi-note url"
+                style="border-radius:6px;border:2px solid #ccc;background-color:#eee;padding: 6px 10px;font-family:'Operator Mono Lig', 'Operator Mono', 'Dank Mono', 'MonoLisa', 'Fira Code', monospace;font-size:14px;cursor:pointer;width:90%;transition:all 0.4s ease-in;">${iconUrl}</pre>
+	    </a>
+	</td>
+</tr>`;
+}).join('\n')}
+</table>
+
+
+</div>
+<div align="center" class="zi-card dark">
+
+[MIT License](https://mit-license.org) · © 2022 [Nicholas Berlette](https://github.com/nberlette/icns)   
+
+</div>
+<br>
+<script type="text/javascript">
+</script>
+
+</body></html>`;
+
+export default async function handler (req: Request, res: Response): Promise<any> {
+  res.setHeader('Content-Type', 'text/html;charset=utf-8');
+  return res.status(200).send(marked(homepage));
+}
