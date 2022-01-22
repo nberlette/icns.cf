6b318efce9f3c754d5a34d85a3d4ac28f6ce6337
Nicholas Berlette <nick@berlette.com>
2022-01-22 02:17:10 -0800

    housekeeping

--- public/api/index.d.ts
+++
@@ -1,2 +0,0 @@
-import type { VercelResponse as Response, VercelRequest as Request } from '@vercel/node';
-export default function handler(req: Request, res: Response): Promise<Response>;
--- public/api/render.d.ts
+++
@@ -1,2 +0,0 @@
-import type { VercelRequest as Request, VercelResponse as Response } from '@vercel/node';
-export default function handler(req: Request, res: Response): Promise<any>;
