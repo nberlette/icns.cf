9d13ea488bdf0309e2f40fb9e6288a4fcced2eeb
Nicholas Berlette <nick@berlette.com>
2022-01-28 09:41:25 -0800

    update api/utils.ts; add color functions

--- api/utils.ts
+++ api/utils.ts
@@ -3,10 +3,54 @@ import type {
   VercelResponse,
   VercelRequestQuery
 } from '@vercel/node';
-import tc from 'tinycolor2';
 
-export { tc };
+/**
+ * Colors.
+ * @link https://npm.im/tinycolor2
+ */
+import tinycolor, { ColorInput, ColorFormats } from 'tinycolor2';
 
+/**
+ * Convert a color to Hexadecimal (3, 4, 6, or 8)
+ * @param {ColorInput} color - the input color to parse.
+ * @param {3|4|6|8} length - the length of hex color to return
+ * @returns {String} hexadecimal string
+ */
+export const toHex = (color: ColorInput, length: 3 | 4 | 6 | 8 = 6): string =>
+  tinycolor(color).toString(`hex${length}`);
+
+/**
+ * Convert a color to RGBA object
+ * @param {ColorInput} color - the input color to parse.
+ * @returns {ColorFormats.RGBA} RGBA object
+ */
+export const toRgb = (color: ColorInput): ColorFormats.RGBA =>
+  tinycolor(color).toRgb();
+
+/**
+ * Convert a color to RGB string (for CSS)
+ * @example toRgbString('f00') -> rgba(255, 0, 0, 1.0)
+ * @param {ColorInput} color - the input color to parse.
+ * @returns {String} RGB string
+ */
+export const toRgbString = (color: ColorInput): string =>
+  tinycolor(color).toString('rgb');
+
+/**
+ * Convert a color to RGBA array.
+ * @example toRgbArray('f00') -> [255, 0, 0, 1]
+ * @param {ColorInput} color - the input color to parse.
+ * @returns {[number, number, number, number]} RGBA array
+ */
+export const toRgbArray = (color: ColorInput): any[] =>
+  Object.values(tinycolor(color).toRgb());
+
+export { tinycolor as tc };
+  
+/**
+ * Query Parameters
+ * @link https://npm.im/@vercel/node
+ */
 export declare type QueryParams = {
   slug?: string,
   name?: string,
@@ -22,7 +66,12 @@ const CACHE_BROWSER = 7 * DAYS; 	// 86400 * 7 days
 const CACHE_CDN = 5 * DAYS;		// 86400 * 5 days
 const CONTENT_TYPE = 'image/svg+xml;charset=utf-8';
 
-export function preflight (res: VercelResponse): void {
+/**
+ * Set preflight settings and headers on our response.
+ * @param {Response} res 
+ * @returns {void}
+ */
+export function preflight (res: Response): void {
   res.setHeader("Access-Control-Allow-Origin", '*');
   res.setHeader("Access-Control-Allow-Methods", 'GET');
   res.setHeader("Access-Control-Allow-Headers", '*');
@@ -38,6 +87,8 @@ export function preflight (res: VercelResponse): void {
 /**
  * Converts a brand title into a slug/filename.
  * @param {String} title The title to convert
+ * @returns {String}
+ * @see {simple-icons}
  */
 export function toSlug (title: string): string {
   return title.toLowerCase()
@@ -61,6 +112,7 @@ export function toSlug (title: string): string {
  * @param {string} svg - our svg string
  * @param {{ fill?: string }} [fill] - the fill color
  * @returns {string} the rewritten svg string
+ * @see {simple-icons}
  */
 
 export function toSVG(svg: string, props: Record<string, any> = {}): string {
@@ -71,7 +123,7 @@ export function toSVG(svg: string, props: Record<string, any> = {}): string {
       'role': 'img'
     },
     props,
-    { 'fill': tc(props.fill).toHex8String() }
+    { 'fill': tinycolor(props.fill).toHex8String() }
   );
   return svg.replace(/^<svg[^>]+>/, `<svg ${<string>(Object.entries(properties).map(([k, v]) => `${k}="${v}"`).join(' '))}>`);
 }
